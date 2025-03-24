75c16b29af537eaeff076a693a33d3b41b5dbc04
HenriqueGennari <henriquegennarilemgruber@gmail.com>
2025-03-24 19:52:53 -0300

    alteração carregar da página

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -4,16 +4,3 @@ console.log(aviso)
 aviso.addEventListener("click",()=>{
     alert("Você será redirecionado para meu currículo!")
 })
-
-// Salvar posição antes do recarregamento
-window.addEventListener("beforeunload", function () {
-    localStorage.setItem("scrollPosition", window.scrollY);
-});
-
-// Restaurar posição após o recarregamento
-window.addEventListener("load", function () {
-    const scrollPosition = localStorage.getItem("scrollPosition");
-    if (scrollPosition) {
-        window.scrollTo(0, scrollPosition);
-    }
-});
